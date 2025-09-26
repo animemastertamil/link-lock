56d98390293bf53d7d70bebd26e7a5aab8021023
animemastertamil <animedubbertamil@gmail.com>
2025-09-26 19:34:54 +0530

    hidden.js

--- hidden/hidden.js
+++ hidden/hidden.js
@@ -13,9 +13,9 @@
  * Display a message in the "alert" area
  */
 function error(text) {
-  const alertText = document.querySelector(".alert");
-  alertText.innerHTML = text;
-  alertText.style.opacity = 1;
+    const alertText = document.querySelector(".alert");
+    alertText.innerHTML = text;
+    alertText.style.opacity = 1;
 }
 
 
@@ -28,72 +28,73 @@ function error(text) {
  * Create a hidden bookmark when the form is filled out.
  */
 async function onHide() {
-  // Fail if the b64 library or API was not loaded
-  if (!("b64" in window && "apiVersions" in window)) {
-    error("Important libraries not loaded!");
-    return;
-  }
+    // Fail if the b64 library or API was not loaded
+    if (!("b64" in window && "apiVersions" in window)) {
+        error("Important libraries not loaded!");
+        return;
+    }
 
-  // Try to get page data from the input hidden URL if possible
-  let urlText = document.querySelector("#encrypted-url").value;
-  let hiddenUrl;
-  try {
-    hiddenUrl = new URL(urlText);
-  } catch {
-    error("Hidden URL is not valid. Make sure it includes \"https://\" too!");
-    return;
-  }
+    // Try to get page data from the input hidden URL if possible
+    let urlText = document.querySelector("#encrypted-url").value;
+    let hiddenUrl;
+    try {
+        hiddenUrl = new URL(urlText);
+    } catch {
+        error("Hidden URL is not valid. Make sure it includes \"https://\" too!");
+        return;
+    }
 
-  // Try to get page data from the input bookmark URL if possible
-  urlText = document.querySelector("#bookmark-url").value;
-  let bookmarkUrl;
-  try {
-    bookmarkUrl = new URL(urlText);
-  } catch {
-    error("Bookmark URL is not valid. Make sure it includes \"https://\" too!");
-    return;
-  }
+    // Try to get page data from the input bookmark URL if possible
+    urlText = document.querySelector("#bookmark-url").value;
+    let bookmarkUrl;
+    try {
+        bookmarkUrl = new URL(urlText);
+    } catch {
+        error("Bookmark URL is not valid. Make sure it includes \"https://\" too!");
+        return;
+    }
 
-  // Ensure that the Link Lock URL is valid
-  let hash = hiddenUrl.hash.slice(1);
-  try {
-    let _ = JSON.parse(b64.decode(hash));
-  } catch {
-    error("The hidden URL appears corrupted. It must be a password-protected Link Lock URL. <a href=\"https://jstrieb.github.io/link-lock\">Click here to add a password.</a>");
-    return;
+    // Ensure that the Link Lock URL is valid
+    let hash = hiddenUrl.hash.slice(1);
+    try {
+        let _ = JSON.parse(b64.decode(hash));
+    } catch {
+        // Corrected link to your 'create' page
+        error("The hidden URL appears corrupted. It must be a password-protected Link Lock URL. <a href=\"https://animastertamil.github.io/link-lock\">Click here to add a password.</a>");
+        return;
 
-    // Uncomment this to allow hiding arbitrary pages. Not secure though, so I
-    // disabled it.
-    /*
-    let hashData = {
-      unencrypted: true,
-      url: hiddenUrl.toString(),
-    };
+        // Uncomment this to allow hiding arbitrary pages. Not secure though, so I
+        // disabled it.
+        /*
+        let hashData = {
+          unencrypted: true,
+          url: hiddenUrl.toString(),
+        };
 
-    hiddenUrl.hash = b64.encode(JSON.stringify(hashData));
-    document.querySelector("#encrypted-url").value = hiddenUrl.toString();
-    */
-  }
+        hiddenUrl.hash = b64.encode(JSON.stringify(hashData));
+        document.querySelector("#encrypted-url").value = hiddenUrl.toString();
+        */
+    }
 
-  let output = document.querySelector("#output");
+    let output = document.querySelector("#output");
 
-  // Set the output href to be the hidden URL with the old URL hash
-  bookmarkUrl.hash = hiddenUrl.hash;
-  output.setAttribute("href", bookmarkUrl.toString());
+    // Set the output href to be the hidden URL with the old URL hash
+    bookmarkUrl.hash = hiddenUrl.hash;
+    output.setAttribute("href", bookmarkUrl.toString());
 
-  // Enable clicking and dragging the output bookmark
-  output.setAttribute("aria-disabled", "false");
+    // Enable clicking and dragging the output bookmark
+    output.setAttribute("aria-disabled", "false");
 
-  // Change the output bookmark title to match the user input
-  output.innerText = document.querySelector("#bookmark-title").value;
+    // Change the output bookmark title to match the user input
+    output.innerText = document.querySelector("#bookmark-title").value;
 
-  error("Bookmark created below.");
+    error("Bookmark created below.");
 
-  // Scroll to the bottom so the user sees where the bookmark was created
-  window.scrollTo({
-    top: document.body.scrollHeight,
-    behavior: "smooth",
-  });
+    // Scroll to the bottom so the user sees where the bookmark was created
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth",
+    });
 }
 
 
@@ -102,18 +103,18 @@ async function onHide() {
  * of the decrypt bookmark via regular expressions.
  */
 function onChangeDecrypt() {
-  let newUrl;
-  try {
-    const newUrlInput = document.querySelector("#decrypt-bookmark-disguise");
-    const _ = new URL(newUrlInput.value);
-    newUrl = newUrlInput.value;
-  } catch (_) {
-    return;
-  }
+    let newUrl;
+    try {
+        const newUrlInput = document.querySelector("#decrypt-bookmark-disguise");
+        const _ = new URL(newUrlInput.value);
+        newUrl = newUrlInput.value;
+    } catch (_) {
+        return;
+    }
 
-  const decryptBookmark = document.querySelector("#decrypt-bookmark");
-  decryptBookmark.href = decryptBookmark.href.replace(/replace\("[^"]*"\)/, `replace("${newUrl}")`);
-  console.log(decryptBookmark.href);
+    const decryptBookmark = document.querySelector("#decrypt-bookmark");
+    decryptBookmark.href = decryptBookmark.href.replace(/replace\("[^"]*"\)/, `replace("${newUrl}")`);
+    console.log(decryptBookmark.href);
 }
 
 
@@ -121,22 +122,22 @@ function onChangeDecrypt() {
  * Get a random link from Wikipedia
  */
 async function randomLink() {
-	let page = await fetch("https://en.wikipedia.org/w/api.php?"
-			+ "format=json"
-			+ "&action=query"
-			+ "&generator=random"
-			+ "&grnnamespace=0" /* Only show articles, not users */
-			+ "&prop=info"
-			+ "&inprop=url" /* Get URLs, they're not there by default */
-			+ "&origin=*") /* https://mediawiki.org/wiki/API:Cross-site_requests */
-		.then(r => r.json())
-		.then(d => {
-			let pages = d.query.pages;
-			return pages[Object.keys(pages)[0]];
-		});
+    let page = await fetch("https://en.wikipedia.org/w/api.php?"
+            + "format=json"
+            + "&action=query"
+            + "&generator=random"
+            + "&grnnamespace=0" /* Only show articles, not users */
+            + "&prop=info"
+            + "&inprop=url" /* Get URLs, they're not there by default */
+            + "&origin=*") /* https://mediawiki.org/wiki/API:Cross-site_requests */
+        .then(r => r.json())
+        .then(d => {
+            let pages = d.query.pages;
+            return pages[Object.keys(pages)[0]];
+        });
 
-  document.querySelector("#bookmark-url").value = await page.canonicalurl;
-  document.querySelector("#bookmark-title").value = await page.title;
+    document.querySelector("#bookmark-url").value = await page.canonicalurl;
+    document.querySelector("#bookmark-title").value = await page.title;
 }
 
 
@@ -146,10 +147,11 @@ async function randomLink() {
  * Run on page load.
  */
 function main() {
-  if (window.location.hash) {
-    document.querySelector("#encrypted-url").value =
-      `https://jstrieb.github.io/link-lock/${window.location.hash}`;
+    if (window.location.hash) {
+        document.querySelector("#encrypted-url").value =
+            // Corrected URL to your 'create' page for autofill
+            `https://animastertamil.github.io/link-lock/create/${window.location.hash}`;
 
-    window.location.hash = "";
-  }
+        window.location.hash = "";
+    }
 }
